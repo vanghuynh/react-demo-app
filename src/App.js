7337ccc70c15c177bf089e6b610ac28f4ff3a997
Huynh Vang <vangh@galaxy.one>
2021-10-01 10:33:00 +0700

    Get expense data from firebase database

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import "./App.css";
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/expenses/NewExpense/NewExpense";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
 import AuthContext from './store/auth-context';
+import Button from './components/ui/Button/Button';
 
 const App = () => {
-  const expenses = [
-    { id: "1", title: "First", amount: 100, date: new Date(2020, 1, 1) },
-    { id: "2", title: "Second", amount: 200, date: new Date() },
-  ];
-  const [expenseList, setExpenseList] = useState(expenses);
-  const saveExpenseHandler = (expense) => {
+  const [expenseList, setExpenseList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // fetch data from server 
+  const fetchDataHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const expenseData = await getExpenseData();
+      setExpenseList(expenseData);
+    } catch (err) {
+      setError('Error getting data');
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchDataHandler();
+  }, [fetchDataHandler]);
+
+  const saveExpenseHandler = async (expense) => {
     console.log("App: ", expense);
     // never work, must use setState
     //expenses.push(expense);
@@ -26,8 +42,25 @@ const App = () => {
       return [modifiedExpense, ...prevState];
     });
     console.log("expenseList: ", expenseList);
+    const response = await fetch('https://g1-mart-demo-default-rtdb.firebaseio.com/expense.json',{
+      method: 'POST',
+      body: JSON.stringify(modifiedExpense)
+    });
+    const data = await response.json();
+    console.log('Expense Data: ', JSON.stringify(data))
   };
   const authCtx = useContext(AuthContext);
+
+  let content = <p>Found no movie</p>
+  if(expenseList.length > 0){
+    content = <Expenses items={expenseList}></Expenses>;
+  }
+  if(isLoading){
+    content = <p>Loading</p>;
+  }
+  if(error){
+    content = <p>{error}</p>;
+  }
 
   return (
     <React.Fragment>
@@ -37,7 +70,10 @@ const App = () => {
       {authCtx.isLoggedIn && 
         <React.Fragment>
           <NewExpense onSaveExpense={saveExpenseHandler}></NewExpense>
-          <Expenses items={expenseList}></Expenses>
+          <Button onClick={fetchDataHandler}>
+            Load Data
+          </Button>
+          {content}
         </React.Fragment>
       }
       </main>
@@ -46,3 +82,22 @@ const App = () => {
 };
 
 export default App;
+
+async function getExpenseData() {
+  const res = await fetch('https://g1-mart-demo-default-rtdb.firebaseio.com/expense.json');
+  if (!res.ok) {
+    throw new Error('Error getting data');
+  }
+  const data = await res.json();
+  console.log(data);
+  const expenseList = Object.entries(data).map(([key,item]) => {
+    return {
+      id: item.id,
+      title: item.title,
+      amount: item.amount,
+      date: new Date(item.date)
+    };
+  });
+  return expenseList;
+}
+
